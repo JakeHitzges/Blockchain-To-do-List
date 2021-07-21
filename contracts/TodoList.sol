7c2adac73426c38cdb8feca01df1e8d9d6734ea3
Jake Hitzges <jakehitzges@gmail.com>
2021-07-21 17:33:54 -0400

    app.js progress

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,4 +3,24 @@ pragma solidity ^0.5.0;
 contract TodoList {
     uint public taskCount = 0;
 
+    struct Task {
+        uint id; 
+        string content;
+        bool completed; 
+    }
+
+ mapping(uint => Task) public tasks;
+
+ constructor() public {
+     createTask("Make a blockchain To-do list");
+     createTask("polish diamond hands");
+     createTask("Go to the moooon!");
+
+ }
+
+ function createTask(string memory _content) public {
+    taskCount ++; 
+    tasks[taskCount] = Task(taskCount, _content, false); 
+ }
+
 }
